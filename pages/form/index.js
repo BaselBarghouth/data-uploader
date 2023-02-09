fd51eded4e102cd5a37de9c9d762b7396f9cec6a
Basel <basel@Basels-MacBook-Pro.local>
2023-02-09 15:29:28 +0100

    ADd map

--- pages/form/index.js
+++ pages/form/index.js
@@ -1,40 +1,40 @@
 import classNames from "classnames";
-const errors = ''
+const errors = "";
 const Form = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-white font-default">
       <main className="w-full max-w-xl">
-    <form >
-      <div className="my-10 flex-col col flex gap-y-10">
-          <label
-            className="mb-2 block text-sm font-medium text-gray-700"
-            htmlFor="email"
-          >
-            Upload data here please
-          </label>
-          <input
-            className={classNames(
-              `block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm`,
-              errors.email && "border-error-500",
+        <form>
+          <div className="my-10 flex-col col flex gap-y-10">
+            <label
+              className="mb-2 block text-sm font-medium text-gray-700"
+              htmlFor="email"
+            >
+              Upload data here please
+            </label>
+            <input
+              className={classNames(
+                `block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-500 focus:ring-primary-500 sm:text-sm`,
+                errors.email && "border-error-500"
+              )}
+              id="email"
+              type="file"
+            />
+            {errors.email && (
+              <p className="mt-2 text-sm text-error-600">
+                {errors.email?.message}
+              </p>
             )}
-            id="email"
-            type="file"
-          />
-          {errors.email && (
-            <p className="mt-2 text-sm text-error-600">
-              {errors.email?.message}
-            </p>
-          )}
-       <button
-    className={classNames(
-      "flex h-12 w-20 place-content-center place-items-center gap-x-3 rounded-md bg-primary-600 px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none enabled:hover:bg-primary-700"
-    )}
-  >
-    Submit
-  </button>
-        </div>
-    </form>
-    </main>
+            <button
+              className={classNames(
+                "flex h-12 w-20 place-content-center place-items-center gap-x-3 rounded-md bg-primary-600 px-4 py-2 text-sm font-medium text-white shadow-sm focus:outline-none enabled:hover:bg-primary-700"
+              )}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 };
--- pages/map/index.js
+++ pages/map/index.js
@@ -1,14 +1,19 @@
-import { useLoadScript, GoogleMap, MarkerF } from '@react-google-maps/api';
-import { useMemo, useState } from 'react';
-
+import { useLoadScript, GoogleMap, MarkerF } from "@react-google-maps/api";
+import { useMemo, useState } from "react";
+import classNames from "classnames";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
-const [count, setCount] = useState(20);
-  const libraries = useMemo(() => ['places'], []);
-  const mapCenter = useMemo(
-    () => ({ lat: 27.672932021393862, lng: 85.31184012689732 }),
-    []
-  );
+  const [count, setCount] = useState(10);
+  const [cordinate, setCordinate] = useState([
+    { lat: 36.2064141, lng: 37.0783733 },
+  ]);
+  const [lat, setLan] = useState("");
+  const [lng, setLon] = useState("");
+
+  const libraries = useMemo(() => ["places"], []);
+  const mapCenter = useMemo(() => ({ lat: 36.2064141, lng: 37.0783733 }), []);
 
   const mapOptions = useMemo(
     () => ({
@@ -20,52 +25,105 @@ const [count, setCount] = useState(20);
   );
 
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyCatnJPPhSFXQMV-5IV-DCExJdcB4V26-g' ,
+    googleMapsApiKey: "AIzaSyCatnJPPhSFXQMV-5IV-DCExJdcB4V26-g",
     libraries: libraries,
   });
 
   if (!isLoaded) {
     return <p>Loading...</p>;
   }
-const cor = [
-    { lat: 27.672932021393862, lng: 85.31184012689732 },
+  const cor = [
+    { lat: 36.2064141, lng: 37.0783733 },
     { lat: 33.672932021393862, lng: 22.31184012689732 },
-    { lat: 23.672932021393862, lng: 34.31184012689732 }
+    { lat: 23.672932021393862, lng: 34.31184012689732 },
+  ];
 
-]
+  const errors = {};
+  const onChangeCounter = (event) => {
+    const {
+      target: { value },
+    } = event;
+    console.log(value);
+    setCount(parseInt(value));
+  };
+  const changeLan = (eve) => {
+    const {
+      target: { value },
+    } = eve;
+    setLan(value);
+  };
+  const changeLong = (eve) => {
+    const {
+      target: { value },
+    } = eve;
+    setLon(value);
+  };
 
-  
-const inc = (event) => {
-  setCount(count + 1);
-};
-
-const dec = () => {
-  setCount(count - 1);
-}
-
+  const addCor = (eve) => {
+    const temp = { lat: parseFloat(lat), lng: parseFloat(lng) };
+    const temp2 = [...cordinate, temp];
+    setCordinate(temp2);
+    console.log(cordinate);
+  };
   return (
-    <div >
-      <div>
-      <div>
-      <button onClick={dec}>-</button>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-white font-default">
+      <main className="w-full max-w-xl">
+        <div className="my-10 flex-col col flex gap-y-10">
+          <div className="flex flex-col gap-y-2">
+            {/* <Form.Group controlId="formFile" className="mb-3">
+        <Form.Label>Default file input example</Form.Label>
+        <Form.Control type="file" />
+ 
+      </Form.Group> */}
 
-      <input type="number" value={count} />
-
-      <button onClick={inc}>+</button>
-    </div>
-      </div>
-   
-      <GoogleMap
-        options={mapOptions}
-        zoom={count}
-        center={mapCenter}
-        mapTypeId={google.maps.MapTypeId.ROADMAP}
-        mapContainerStyle={{ width: '800px', height: '800px' }}
-        onLoad={() => console.log('Map Component Loaded...')}
-      >
-        {cor && cor.map((data,index)=> <MarkerF position={data} onLoad={() => console.log('Marker Loaded')} />)}
-       
-        </GoogleMap>
+            <Form.Label htmlFor="inputPassword5">Lan</Form.Label>
+            <Form.Control
+              type="text"
+              value={lat}
+              id="inputPassword5"
+              name="lan"
+              aria-describedby="passwordHelpBlock"
+              onChange={(event) => changeLan(event)}
+            />
+            <Form.Label htmlFor="inputPassword5">Long</Form.Label>
+            <Form.Control
+              value={lng}
+              type="text"
+              name="lon"
+              id="inputPassword5"
+              aria-describedby="passwordHelpBlock"
+              onChange={(event) => changeLong(event)}
+            />
+            <Button onClick={(event) => addCor(event)} variant="primary">
+              Add cordinate
+            </Button>
+          </div>
+          <div>
+            <Form.Label>Zoom</Form.Label>
+            <Form.Range
+              value={count}
+              onChange={(event) => onChangeCounter(event)}
+            />
+          </div>
+          <GoogleMap
+            options={mapOptions}
+            zoom={5}
+            center={mapCenter}
+            mapTypeId={google.maps.MapTypeId.ROADMAP}
+            mapContainerStyle={{ width: "800px", height: "800px" }}
+            onLoad={() => console.log("Map Component Loaded...")}
+          >
+            {cordinate &&
+              cordinate.map((data, index) => (
+                <MarkerF
+                  key={index}
+                  position={data}
+                  onLoad={() => console.log("Marker Loaded")}
+                />
+              ))}
+          </GoogleMap>
+        </div>
+      </main>
     </div>
   );
 };
